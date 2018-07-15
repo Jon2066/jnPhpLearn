d573333a3502e06d6429b9ea7abdc4251f593c4c
jonathan <jonathankeller@sina.com>
2018-07-15 11:23:22 +0800

    path

--- index.php
+++ index.php
@@ -7,4 +7,15 @@
  */
 
     echo "<p>git test</p>";
+
+    $t = trim(__DIR__.'/');
+    echo  "</br>";
+    echo "<p>$t</p>";
+    echo  "</br>";
+    $root = $_SERVER['SCRIPT_NAME'];
+    $request = $_SERVER['REQUEST_URI'];
+    echo "<p>$root</p>";
+    echo  "</br>";
+    echo "<p>$request</p>";
+
 ?>
