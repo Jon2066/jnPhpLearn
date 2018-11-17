621a200a4c2706115f3d7159a30b03e1b7fe8b33
jonathan <jonathankeller@sina.com>
2018-11-17 21:27:42 +0800

    格式修改

--- live_list.php
+++ live_list.php
@@ -11,17 +11,31 @@ $list = array(
     array(
         "title"=>"内地",
         "data" => array(
-        "CCTV-1"=>"http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8",
-        "CCTV-2"=>"http://ivi.bupt.edu.cn/hls/cctv2hd.m3u8",
-        "CCTV-6"=>"http://ivi.bupt.edu.cn/hls/cctv6hd.m3u8",
-        "CCTV-7"=>"http://ivi.bupt.edu.cn/hls/cctv7hd.m3u8"
+            array(
+                "name"=>"CCTV-1",
+                "url"=>"http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8"
+            ),
+            array(
+                "name"=>"CCTV-2",
+                "url"=>"http://ivi.bupt.edu.cn/hls/cctv2hd.m3u8"
+            ),
+            array(
+                "name"=>"CCTV-6",
+                "url"=>"http://ivi.bupt.edu.cn/hls/cctv6hd.m3u8"
+            ),
         )
     ),
     array(
         "title"=>"香港",
         "data"=> array(
-        "成龙影视" =>"http://aldirect.hls.huya.com/huyalive/94525224-2460685722-10568564701724147712-2789253838-10057-A-0-1_1200.m3u8",
-        "翡翠台"=>"http://pullhls6.inke.cn/live/1542196929080215/playlist.m3u8",
+            array(
+                "name"=>"成龙影视",
+                "url"=>"http://aldirect.hls.huya.com/huyalive/94525224-2460685722-10568564701724147712-2789253838-10057-A-0-1_1200.m3u8"
+            ),
+            array(
+                "name"=>"翡翠台",
+                "url"=>"http://pullhls6.inke.cn/live/1542196929080215/playlist.m3u8"
+            ),
         )
     )
 );
